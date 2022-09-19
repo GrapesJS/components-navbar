6b4fd60672379e8fa925f4af77f706493a439b6c
Artur Arseniev <artur.catch@hotmail.it>
2022-09-19 13:31:37 +0400

    Clean blocks file

--- src/blocks.ts
+++ src/blocks.ts
@@ -4,96 +4,6 @@ import { RequiredPluginOptions } from '.';
 export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
   const { block, label, id } = opts;
 
-  /*
-  const navbarPfx = opts.navbarClsPfx || 'navbar';
-  const style = opts.defaultStyle ? `
-  <style>
-    .${navbarPfx}-items-c {
-      display: inline-block;
-      float: right;
-    }
-
-    .${navbarPfx} {
-      background-color: #222;
-      color: #ddd;
-      min-height: 50px;
-      width: 100%;
-    }
-
-    .${navbarPfx}-container {
-      max-width: 950px;
-      margin: 0 auto;
-      width: 95%;
-    }
-
-    .${navbarPfx}-container::after {
-      content: "";
-      clear: both;
-      display: block;
-    }
-
-    .${navbarPfx}-brand {
-      vertical-align: top;
-      display: inline-block;
-      padding: 5px;
-      min-height: 50px;
-      min-width: 50px;
-      color: inherit;
-      text-decoration: none;
-    }
-
-    .${navbarPfx}-menu {
-      padding: 10px 0;
-      display: block;
-      float: right;
-      margin: 0;
-    }
-
-    .${navbarPfx}-menu-link {
-      margin: 0;
-      color: inherit;
-      text-decoration: none;
-      display: inline-block;
-      padding: 10px 15px;
-    }
-
-    .${navbarPfx}-burger {
-      margin: 10px 0;
-      width: 45px;
-      padding: 5px 10px;
-      display: none;
-      float: right;
-      cursor: pointer;
-    }
-
-    .${navbarPfx}-burger-line {
-      padding: 1px;
-      background-color: white;
-      margin: 5px 0;
-    }
-
-    @media (max-width: 768px) {
-      .${navbarPfx}-burger {
-        display: block;
-      }
-
-      .${navbarPfx}-items-c {
-        display: none;
-        width: 100%;
-      }
-
-      .${navbarPfx}-menu {
-        width: 100%;
-      }
-
-      .${navbarPfx}-menu-link {
-        display: block;
-      }
-    }
-  </style>
-  ` : '';
-  */
-
   if (block) {
     editor.Blocks.add(id, {
       media: `<svg viewBox="0 0 24 24">
@@ -104,34 +14,6 @@ export default (editor: grapesjs.Editor, opts: RequiredPluginOptions) => {
       select: true,
       content: { type: id },
       ...block,
-      /*
-      content: `
-        <div class="${navbarPfx}" data-gjs-droppable="false" data-gjs-custom-name="${c.labelNavbar}" data-gjs="${navbarRef}">
-          <div class="${navbarPfx}-container" data-gjs-droppable="false" data-gjs-draggable="false"
-            data-gjs-removable="false" data-gjs-copyable="false" data-gjs-highlightable="false"
-            data-gjs-custom-name="${c.labelNavbarContainer}">
-
-            <a href="/" class="${navbarPfx}-brand" data-gjs-droppable="true"></a>
-
-            <div class="${navbarPfx}-burger" data-gjs-type="burger-menu">
-              <div class="${navbarPfx}-burger-line" data-gjs-custom-name="${c.labelBurgerLine}" data-gjs-droppable="false" data-gjs-draggable="false"></div>
-              <div class="${navbarPfx}-burger-line" data-gjs-custom-name="${c.labelBurgerLine}" data-gjs-droppable="false" data-gjs-draggable="false"></div>
-              <div class="${navbarPfx}-burger-line" data-gjs-custom-name="${c.labelBurgerLine}" data-gjs-droppable="false" data-gjs-draggable="false"></div>
-            </div>
-
-            <div class="${navbarPfx}-items-c" data-gjs="${navbarItemsRef}">
-              <nav class="${navbarPfx}-menu" data-gjs="${menuRef}" data-gjs-custom-name="${c.labelMenu}">
-                <a href="#" class="${navbarPfx}-menu-link" data-gjs-custom-name="${c.labelMenuLink}" data-gjs-draggable="[data-gjs=${menuRef}]">${c.labelHome}</a>
-                <a href="#" class="${navbarPfx}-menu-link" data-gjs-custom-name="${c.labelMenuLink}" data-gjs-draggable="[data-gjs=${menuRef}]">${c.labelAbout}</a>
-                <a href="#" class="${navbarPfx}-menu-link" data-gjs-custom-name="${c.labelMenuLink}" data-gjs-draggable="[data-gjs=${menuRef}]">${c.labelContact}</a>
-              </nav>
-            </div>
-
-          </div>
-        </div>
-        ${style}
-      `,
-      */
     });
   }
 }
