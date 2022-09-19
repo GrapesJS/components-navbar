974e4ec37cc3fe14bfa0895bdac1db04b7f63ccb
Artur Arseniev <artur.catch@hotmail.it>
2022-09-19 11:53:54 +0400

    Update component api

--- src/components.js
+++ src/components.js
@@ -1,14 +1,10 @@
 export default (editor, opt = {}) => {
   const c = opt;
-  const dc = editor.DomComponents;
-  const defaultType = dc.getType('default');
-  const defaultModel = defaultType.model;
   const burgerType = 'burger-menu';
 
-  dc.addType(burgerType, {
-    model: defaultModel.extend({
+  editor.Components.addType(burgerType, {
+    model: {
       defaults: {
-        ...defaultModel.prototype.defaults,
         'custom-name': c.labelBurger,
         draggable: false,
         droppable: false,
@@ -119,14 +115,6 @@ export default (editor, opt = {}) => {
           this[stringCollapse] = 1;
         },
       },
-    }, {
-      isComponent(el) {
-        if(el.getAttribute &&
-          el.getAttribute('data-gjs-type') == burgerType) {
-          return {type: burgerType};
-        }
-      },
-    }),
-    view: defaultType.view,
+    },
   });
 }
